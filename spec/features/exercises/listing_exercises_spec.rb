b249e2b474cea6a16366f0f4d7c874f618ed004b
Rhonda Tinkham <rtinkham@live.com>
2018-01-27 05:24:12 +0000

    Display user's workout

---
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.feature "Listing Exercises" do
+  before do
+    @patty = User.create(email: "patty@example.com", password: "password")
+    login_as(@patty)
+    
+    @e1 = @patty.exercises.create(duration_in_min: 20,
+                                  workout: "Dancing",
+                                  workout_date: Date.today)
+    
+    @e2 = @patty.exercises.create(duration_in_min: 60,
+                                  workout: "Thred Mill",
+                                  workout_date: Date.today)
+  end
+  
+  scenario "shows user's workout for last 7 days" do
+    visit '/'
+    
+    click_link "My Lounge"
+    
+    expect(page).to have_content(@e1.duration_in_min)
+    expect(page).to have_content(@e1.workout)
+    expect(page).to have_content(@e1.workout_date)
+    
+    expect(page).to have_content(@e2.duration_in_min)
+    expect(page).to have_content(@e2.workout)
+    expect(page).to have_content(@e2.workout_date)
+    
+    # expect(page).not_to have_content(@e3.duration_in_min)
+    # expect(page).not_to have_content(@e3.workout)
+    # expect(page).not_to have_content(@e3.workout_date)
+  end
+end
