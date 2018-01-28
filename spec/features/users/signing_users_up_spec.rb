0f1a54494df71737ae2db7bc2ca1823c6ea97fe6
Rhonda Tinkham <rtinkham@live.com>
2018-01-28 04:31:25 +0000

    Add first and last names to sign up form

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -6,12 +6,17 @@ RSpec.feature "Users signup" do
     visit "/"
    
     click_link "Sign up"
+    fill_in "First name", with: "Patty"
+    fill_in "Last name", with: "Arquette"
     fill_in "Email", with: "patty@example.com"
     fill_in "Password",  with: "password"
     fill_in "Password confirmation",  with: "password"
     click_button "Sign up"
    
     expect(page).to have_content("You have signed up successfully.")
+    
+    expect(page).to have_content("Patty Arquette")
  
   end
+  
 end
